8920bf1e088e3e362e3b19850b172081601db57a
yashraj <yashraj44777@gmail.com>
2023-03-18 19:23:14 +0530

    navbar branch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/DashBoard";
+import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Dashboard />}></Route>
       </ Routes>
