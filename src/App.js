d868bc9ee96c1fd37e9657a86744a03eabb34266
dev-064 <divyanshujha112@gmail.com>
2023-03-18 20:17:20 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/DashBoard";
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Dashboard />}></Route>
+        <Route path='/' element={<Dashboard />}/>
+        <Route path='/login' element={<Login />}/>
       </ Routes>
     </div>
   );
