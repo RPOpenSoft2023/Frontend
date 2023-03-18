78b2e35f5914697408058c062277aff04bd3dc50
Ashutosh Singh <ashutosh@Ashutoshs-MacBook-Air.local>
2023-03-18 21:44:19 +0530

    Initial Commit

---
+++ src/Data/TableData.js
@@ -0,0 +1,63 @@
+const TableData=[
+    {
+        key: '1',
+        date: '2021-01-01',
+        desc: 'Salary',
+        debit: 0,
+        credit: 10000,
+        balance: 10000,
+    },
+    {
+        key: '2',
+        date: '2021-01-02',
+        desc: 'Rent',
+        debit: 1000,
+        credit: 0,
+        balance: 9000,
+    },
+    {
+        key: '3',
+        date: '2021-01-03',
+        desc: 'Electricity Bill',
+        debit: 500,
+        credit: 0,
+        balance: 8500,
+    },
+    {
+        key: '4',
+        date: '2021-01-04',
+        desc: 'Grocery',
+        debit: 1000,
+        credit: 0,
+        balance: 7500,
+    },
+    {
+        key: '5',
+        date: '2021-01-05',
+        desc: 'Shopping',
+        debit: 2000,
+        credit: 0,
+        balance: 5500,
+    },
+    {
+        key: '6',
+        date: '2021-01-06',
+        desc: 'Food',
+        debit: 500,
+        credit: 0,
+        balance: 5000,
+    },
+    {
+        key: '7',
+        date: '2021-01-07',
+        desc: 'Shopping',
+        debit: 2000,
+        credit: 0,
+        balance: 3000,
+    },
+
+
+
+
+]
+export default TableData;
--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -1,8 +1,48 @@
+import AnalyserCard from "../Components/AnalyserCard"
+import CardData from "../Data/CardData"
+import TableData from "../Data/TableData";
+import ColumnData from "../Data/ColumnData";
+import AccountData from "../Data/AccountData";
+import UserData from "../Data/UserData";
+import { Row, Col, Card, Table } from 'antd'
 const Dashboard = () => {
     return (
         <div>
-            <h1>DashBoard</h1>
-            <p>this is us</p>
+            <div style={{ display: 'flex' }}>
+                {CardData.map((data) =>
+                    <AnalyserCard
+                        key={data.key}
+                        id={data.key}
+                        data={data.num}
+                        title={data.title}
+                    />
+                )}
+            </div>
+            <div>
+                <Row justify="space-between">
+                    <Col span={8}>
+                        <Card title="Bank Details" bordered={false} style={{ width: '100%', height: '100%'}}>
+                            <p>Bank Name: {AccountData.bankName}</p>
+                            <p>Account Number: {AccountData.accountNumber}</p>
+                            <p>Account Type: {AccountData.accountType}</p>
+                            <p>Branch Name: {AccountData.branchName}</p>
+                            <p>Branch Address: {AccountData.branchAddress}</p>
+                            <p>IFSC Code: {AccountData.ifscCode}</p>
+                            <p>Account Holder Name: {UserData.name}</p>
+                            <p>Account Holder Address: {UserData.address}</p>
+                            <p>Account Holder Phone Number: {UserData.phone}</p>
+                            <p>Account Holder Email: {UserData.email}</p>
+                        </Card>
+                    </Col>
+                    <Col span={16}>
+                        <Card title="Transactions" bordered={false} style={{ width: '100%', height: '100%', textAlign:"center" }}>
+                            <Table columns={ColumnData} dataSource={TableData}
+                                pagination={{ pageSize: 5 }}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 }
