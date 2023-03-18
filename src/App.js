f949b59080b603cb678bf684cf2a7d92541dd815
shivamkgpian <shivamkval@gmail.com>
2023-03-18 23:11:44 +0530

    Login Signup page created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/DashBoard";
-import Navbar from './Components/Navbar/navbar';
+import SignupPage from './Pages/Signup';
+import LoginPage from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Routes>
         <Route path='/' element={<Dashboard />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/signup' element={<SignupPage />}></Route>
       </ Routes>
     </div>
   );
