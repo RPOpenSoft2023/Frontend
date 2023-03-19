0c85050199a3e474606406c726c5dff1d98cc873
AdityaKumar77 <adityadarak59@gmail.com>
2023-03-19 14:47:37 +0530

    analyser page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Analyser from "./Pages/Analyser";
+
+function App() {
+  return (
+    <div className="App bg-gray-200">
+      <Routes>
+        <Route path='' element={<Analyser />}></Route>
+      </ Routes>
+    </div>
+  );
+}
+
+export default App;
