62829b134153a383d57ce126b8f8068cd586ef99
dev-064 <divyanshujha112@gmail.com>
2023-03-19 22:30:09 +0530

    .

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,7 @@ export default function Header({
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 {heading}
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600 mt-5">
+            <p className=" text-center text-sm text-gray-600 mt-5">
                 {paragraph} {' '}
                 <Link to={linkUrl} className="font-medium text-sky-600 hover:text-sky-500">
                     {linkName}
