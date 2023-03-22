4249c192664c72e8a2979878f33342f409ae7f68
AdityaKumar77 <adityadarak59@gmail.com>
2023-03-22 22:31:57 +0530

    Graphs added

---
+++ src/Data/MonthlySummaryData.js
@@ -0,0 +1,28 @@
+const MonthlySummaryData = [
+    {
+        title: 'January',
+        dataIndex: 'date',
+        key: 'date',
+    },
+    {
+        title: 'February',
+        dataIndex: 'desc',
+        key: 'desc',
+    },
+    {
+        title: 'March',
+        dataIndex: 'debit',
+        key: 'debit',
+    },
+    {
+        title: 'April',
+        dataIndex: 'credit',
+        key: 'credit',
+    },
+    {
+        title: 'March',
+        dataIndex: 'balance',
+        key: 'balance',
+    },
+]
+export default MonthlySummaryData;
--- src/Pages/Analyser.js
+++ src/Pages/Analyser.js
@@ -2,19 +2,14 @@ import AnalyserCard from "../Components/AnalyserCard"
 import CardData from "../Data/CardData"
 import TableData from "../Data/TableData";
 import ColumnData from "../Data/ColumnData";
-import AccountData from "../Data/AccountData";
-import UserData from "../Data/UserData";
-import { Descriptions } from 'antd';
-// import { Descriptions } from 'antd';
 import { Row, Col, Card, Table } from 'antd'
-import PieChart from "../Components/PieChart";
+import MonthlySummaryData from '../Data/MonthlySummaryData';
+import MonthlySummaryTableData from '../Data/MonthlySummaryTable';
 import Plot from "../Components/Plot";
 import Line from "../Components/Line"
 import { Tabs } from 'antd';
 import styled from 'styled-components';
-const change = (key) => {
-    console.log(key)
-}
+import LineChart from '../Components/LineChart';
 
 const Analyser = () => {
     const items = [
@@ -33,75 +28,11 @@ const Analyser = () => {
                         />
                     )}
                 </Container>
-
-                <div className='bg-white' style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Card bordered={true} style={{ width: '50%', height: '50%', fontSize: "16px", textAlign: "center" }}>
-                        <Descriptions
-                            bordered
-                            column={{
-                                xxl: 4,
-                                xl: 1,
-                                lg: 3,
-                                md: 3,
-                                sm: 2,
-                                xs: 1,
-                            }}
-                            responsive={{
-                                xxl: 'undefined',
-                                xl: 2,
-                                lg: 2,
-                                md: 2,
-                                sm: 1,
-                                xs: 1,
-                            }}
-                        >
-                            <Descriptions.Item label={<b>Bank Name</b>} >{AccountData.bankName}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Number</b>} >{AccountData.accountNumber}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Type</b>} >{AccountData.accountType}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Branch Name</b>} >{AccountData.branchName}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Branch Address</b>} >{AccountData.branchAddress}</Descriptions.Item>
-                            <Descriptions.Item label={<b>IFSC Code</b>} >{AccountData.ifscCode}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Holder's Name</b>} >{UserData.name}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Holder's Address</b>} >{UserData.address}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Phone Number</b>} >{UserData.phone}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Email</b>} >{UserData.email}</Descriptions.Item>
-
-
-
-                        </Descriptions>
-
-                    </Card>
-                    <Card bordered={true} style={{ width: '50%', height: '50%', fontSize: "16px", textAlign: "center" }}>
-                        <Descriptions
-                            bordered
-                            column={{
-                                xxl: 4,
-                                xl: 1,
-                                lg: 3,
-                                md: 3,
-                                sm: 2,
-                                xs: 1,
-                            }}
-                        >
-                            <Descriptions.Item label={<b>Bank Name</b>} >{AccountData.bankName}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Number</b>} >{AccountData.accountNumber}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Type</b>} >{AccountData.accountType}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Branch Name</b>} >{AccountData.branchName}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Branch Address</b>} >{AccountData.branchAddress}</Descriptions.Item>
-                            <Descriptions.Item label={<b>IFSC Code</b>} >{AccountData.ifscCode}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Holder's Name</b>} >{UserData.name}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Account Holder's Address</b>} >{UserData.address}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Phone Number</b>} >{UserData.phone}</Descriptions.Item>
-                            <Descriptions.Item label={<b>Email</b>} >{UserData.email}</Descriptions.Item>
-
-                        </Descriptions>
-
-                    </Card>
-                </div>
+                <LineChart/>
                 <Row justify="space-between" className="mx-3">
                     <Col span={12}>
                         <div className='bg-white' style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100%' }}>
-                            <PieChart/>
+                            {/* <PieChart/> */}
                         </div>
                     </Col>
                     <Col span={12} >
@@ -141,38 +72,21 @@ const Analyser = () => {
                 </Card>
             </div>,
         },
-        // {
-        //     key: '3',
-        //     label: `Pie Chart`,
-        //     children: <Row justify="space-between" className="mx-3">
-        //         <Col span={11}>
-        //             <div className='bg-white' style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100%' }}>
-        //                 <PieChart></PieChart>
-        //             </div>
-        //         </Col>
-        //         <Col span={13} >
-        //             <div className='bg-white' style={{ display: 'flex', justifyContent: 'center', widht: '100%', height: '100%' }}><Card bordered={false} style={{ width: '100%', height: '100%', textAlign: "center" }}>
-        //                 <Plot />
-        //             </Card>
-        //             </div>
-        //         </Col>
-        //     </Row>
-        //     ,
-        // },
+        {
+            key: '3',
+            label: `Monthly Summary`,
+            children: <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card bordered={true} style={{ width: '60%', height: 'auto', textAlign: "center" }}>
+                    <Table columns={MonthlySummaryData} dataSource={MonthlySummaryTableData}
+                        pagination={{ pageSize: 9 }}
+                    />
+                </Card>
+            </div>,
+        },
 
     ];
     return (
         <div>
-            {/* <div style={{ display: 'flex', justifyContent: 'center' }} className="my-3">
-                {CardData.map((data) =>
-                    <AnalyserCard
-                        key={data.key}
-                        id={data.key}
-                        data={data.num}
-                        title={data.title}
-                    />
-                )}
-            </div> */}
             <Tabs animated defaultActiveKey="1" centered items={items} />
 
         </div>
