05d8aa2ad2e34f9b403083e158cb7567e2ff2b97
dev-064 <divyanshujha112@gmail.com>
2023-03-23 20:29:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import BankingDetails from "./Pages/BankingDetails";
 import Analyser from "./Pages/Analyser";
 import Join from "./Components/join";
 import Private from "./Components/private";
+import Private_Signup from "./Components/Private_Signup";
 function App() {
 
   return (
@@ -19,7 +20,7 @@ function App() {
         <Route path='/' element={<LoginPage />}></Route>
         <Route path='/dashboard' element={<Private><Dashboard /></Private>}></Route>
         <Route path='/signup' element={<BeforeSignUpPage />}></Route>
-        <Route path='/signup/profile' element={<SignupPage />}></Route>
+        <Route path='/signup/profile' element={<Private_Signup><SignupPage /></Private_Signup>}></Route>
         <Route path='/analyser' element={<Analyser />}></Route>
         <Route path='/banking' element={<Private><BankingDetails /></Private>}></Route>
         <Route path='/join' element={<Join />}></Route>
